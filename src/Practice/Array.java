d9356e88dfe4a6b546ac3fe1923b3a8bed12b7b2
KITCOOP <KITCOOP@DESKTOP-428VTGC>
2019-08-30 18:03:26 +0900

    2019-08-30

--- src/Practice/Array.java
+++ src/Practice/Array.java
@@ -5,5 +5,16 @@ public class Array {
 		int[] a = {1,2,3,4};
 		System.out.println(a[1]);
 		
+		System.out.println(2+7+""+7);
+		
+		boolean isSuccess = true;
+		System.out.printf("%b%n",isSuccess);
+		
+		int c = 123;
+		
+		String d =  "scriptElement";
+		System.out.printf("[%s]",c);
+		
+		
 	}
 }
